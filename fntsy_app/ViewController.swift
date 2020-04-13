41f3e31655d562e7ecce190b9638c5134fbb1ba9
snie11 <selinanie11@gmail.com>
2020-04-12 22:49:53 -0500

    Initial Commit

---
+++ fntsy_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fntsy_app
+//
+//  Created by Selina Nie on 4/12/20.
+//  Copyright © 2020 Selina Nie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
